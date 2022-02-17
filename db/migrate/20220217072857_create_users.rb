ff5e3c14c22f41dc7ada50ce71ef77aed05e4657
nokiPro <noki.g2000@gmail.com>
2022-02-17 16:54:39 +0900

    Userモデルのemailの型が間違っていたため修正した。また、testがこけるので一旦消した

--- db/migrate/20220217072857_create_users.rb
+++ db/migrate/20220217072857_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.integer :email, null: false
+      t.string :email, null: false
       t.integer :capacity, null: false, default: 0
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_02_17_072857) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "email", null: false
+    t.string "email", null: false
     t.integer "capacity", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
