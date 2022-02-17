8a6db616ac9fca6c0a3e3c400f731ddd1835ea3d
nokiPro <noki.g2000@gmail.com>
2022-02-17 16:39:07 +0900

    Userモデルを作成し、マイグレーションを実行した

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_17_072857) do
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "email", null: false
+    t.integer "capacity", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
